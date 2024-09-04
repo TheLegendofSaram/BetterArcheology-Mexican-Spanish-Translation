64366e3be55ad4549577b976ce76ca7612a0a593
Ruven <ruven-bellmann@web.de>
2024-09-04 23:01:01 +0300

    fix: Tunneling not breaking recursively

--- src/main/java/net/Pandarix/betterarcheology/mixin/TunnelingEnchantmentMixin.java
+++ src/main/java/net/Pandarix/betterarcheology/mixin/TunnelingEnchantmentMixin.java
@@ -2,12 +2,14 @@ package net.Pandarix.betterarcheology.mixin;
 
 import net.Pandarix.betterarcheology.BetterArcheologyConfig;
 import net.Pandarix.betterarcheology.enchantment.ModEnchantments;
+import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
+import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.MiningToolItem;
-import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
@@ -27,13 +29,31 @@ public class TunnelingEnchantmentMixin
         {
             //if the tool is right for the block that should be broken
             //if the difference of the hardness of the block below is not more than 3,75
-            BlockState blockStateBelow = world.getBlockState(pos.down());
+            BlockPos ba$downPos = pos.down();
+            BlockState ba$blockStateBelow = world.getBlockState(ba$downPos);
 
-            if (stack.isSuitableFor(state) && stack.isSuitableFor(blockStateBelow) && Math.abs((world.getBlockState(pos.down()).getHardness(world, pos.down()) - world.getBlockState(pos).getHardness(world, pos))) <= 3.75)
+            if (stack.isSuitableFor(state) && stack.isSuitableFor(ba$blockStateBelow) && Math.abs((world.getBlockState(ba$downPos).getHardness(world, ba$downPos) - world.getBlockState(pos).getHardness(world, pos))) <= 3.75)
             {
-                if (miner instanceof ServerPlayerEntity serverPlayer)
+                if (miner instanceof PlayerEntity player)
                 {
-                    serverPlayer.interactionManager.tryBreakBlock(pos.down());
+                    BlockEntity ba$blockEntityBelow = world.getBlockEntity(ba$downPos);
+                    Block ba$block = ba$blockStateBelow.getBlock();
+                    ba$block.onBreak(world, ba$downPos, ba$blockStateBelow, player);
+                    boolean ba$removeSuccess = world.removeBlock(ba$downPos, false);
+                    if (ba$removeSuccess)
+                    {
+                        ba$block.onBroken(world, ba$downPos, ba$blockStateBelow);
+                    }
+
+                    if (!player.isCreative())
+                    {
+                        ItemStack ba$stackCopy = stack.copy();
+                        boolean bl2 = player.canHarvest(ba$blockStateBelow);
+                        if (ba$removeSuccess && bl2)
+                        {
+                            ba$block.afterBreak(world, player, ba$downPos, ba$blockStateBelow, ba$blockEntityBelow, ba$stackCopy);
+                        }
+                    }
                 }
             }
         }
